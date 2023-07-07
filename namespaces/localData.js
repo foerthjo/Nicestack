4f4caa5cabbb712b4afcc10b7a441fa16f94a3e0
Johannes Förth <johannes.foerth@web.de>
2023-07-07 20:39:28 +0200

    made localData responsible for global cookies

--- namespaces/localData.js
+++ namespaces/localData.js
@@ -24,6 +24,7 @@
 @client setCookiesAllowed(allowed) {
 	if (this.get('cookiesAllowed') !== allowed) {
 		this.set('cookiesAllowed', allowed);
+		window.cookiesAllowed = allowed;
 		if (allowed === false) {
 			localStorage.clear();
 		} else {
